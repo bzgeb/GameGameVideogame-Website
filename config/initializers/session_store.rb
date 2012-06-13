942930ef195f0df7cfca661cca5c3bfa23cc2f55
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-06-13 14:35:17 -0400

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GameGameVideogame::Application.config.session_store :cookie_store, key: '_game-game-videogame_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GameGameVideogame::Application.config.session_store :active_record_store
